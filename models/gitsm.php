20f5418aa85f9744f9155b29780d2b02ecd37c8a
Brandon Hale <admin@local.host>
2014-06-29 09:20:19 -0400

    add sites endpoint, refactor gitsm models

---
+++ models/gitsm.php
@@ -0,0 +1,22 @@
+<?php
+
+class GitsmDB {
+  function __construct() {
+    // Connect to GITSM DB
+    $host = $_ENV["GITSM_DB_HOST"];
+    $user = $_ENV["GITSM_DB_USER"];
+    $password = $_ENV["GITSM_DB_PASSWORD"];
+    $database = $_ENV["GITSM_DB_DATABASE"];
+
+    mssql_connect($host, $user, $password) or die ("Could not connect to MSSQL");
+    mssql_select_db($database);
+  }
+
+  function findOne($query) {
+    $result = mssql_query($query);
+    $data = mssql_fetch_array($result);
+    return $data;
+  }
+}
+
+?>
