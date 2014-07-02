a8b479487239233f77d129cc74ecc6109ef6dde1
Brandon <bhale@users.noreply.github.com>
2014-07-02 11:35:00 -0400

    Update gitsm.php

--- models/gitsm.php
+++ models/gitsm.php
@@ -3,10 +3,10 @@
 class GitsmDB {
   function __construct() {
     // Connect to GITSM DB
-    $host = $_ENV["GITSM_DB_HOST"];
-    $user = $_ENV["GITSM_DB_USER"];
-    $password = $_ENV["GITSM_DB_PASSWORD"];
-    $database = $_ENV["GITSM_DB_DATABASE"];
+    $host = getenv('GITSM_DB_HOST');
+    $user = getenv('GITSM_DB_USER');
+    $password = getenv('GITSM_DB_PASSWORD');
+    $database = getenv('GITSM_DB_DATABASE');
 
     mssql_connect($host, $user, $password) or die ("Could not connect to MSSQL");
     mssql_select_db($database);
